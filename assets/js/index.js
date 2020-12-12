09d48b9112496cd82b2e481221c2cd162a0495e6
Igor Kroshkin <igor.krosh@gmail.com>
2020-12-13 00:20:26 +0300

    Секция Интерфейсов - Десктоп

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,10 @@ $(document).ready(Core);
 
 function Core()
 {
+    
     SetMainSlider();
+    
+    SetInterfaceSection();
 }
 
 function SetMainSlider()
@@ -16,4 +19,53 @@ function SetMainSlider()
         navText: ['', ''],
         loop: true
     });
+}
+
+function SetInterfaceSection()
+{
+    
+    SetWhiteLinePosition($('.btn-interface')[0]);
+    SetPanelInterface('cooling');
+
+    $('.btn-interface').on('click', function() {
+        SetWhiteLinePosition(this);
+        SetPanelInterface($(this).attr('interface'));
+    })
+}
+
+function SetWhiteLinePosition(btn)
+{
+    let lineWidth = $(btn).outerWidth() * 0.8;
+    let left = btn.offsetLeft + ($(btn).outerWidth() - lineWidth) / 2
+
+    $('.white-line').css({
+        left: left,
+        width: lineWidth
+    });
+}
+
+function SetPanelInterface(interfaceName)
+{
+    $('.interface [interface].active').animate({
+        opacity: 0
+    }, 500, function() {
+        $(this).removeClass('active');
+    })
+
+    $(`.interface-text.active`).animate({
+        opacity: 0
+    }, 500, function() {
+        $(this).removeClass('active');
+        $(`.interface-text[interface="${interfaceName}"]`).addClass('active');
+        $(`.interface-text[interface="${interfaceName}"]`).animate({
+            opacity: 1
+        }, 500);
+    })
+
+    $(`.interface [interface="${interfaceName}"]`).addClass('active');
+    $(`.interface [interface="${interfaceName}"]`).animate({
+        opacity: 1
+    }, 500);
+
+    
 }
